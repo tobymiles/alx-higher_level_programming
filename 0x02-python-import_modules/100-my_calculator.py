f71974b2ba0a15ae921e75e4cbd1d8c4cc08d062
Ekwealor Tobechukwu <ekwealortobechukwu22@gmail.com>
2023-03-10 12:29:47 -0800

    0x02 python Task-modified

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -22,4 +22,3 @@ if __name__ == "__main__":
         print('Unknown operator. Available operators: +, -, * and /')
         exit(1)
     exit(0)
-..........................................................
--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -17,4 +17,3 @@ if __name__ == "__main__":
     else:
         print("{} argument:".format(size))
         print("{}: {}".format(size, arg[1]))
-.................................................
